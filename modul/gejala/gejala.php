d594abdb3a1c3594b056900c60f33833862c461e
ramdanks21 <ramdankusumah2@gmail.com>
2023-03-02 08:19:38 +0700

    update gejala

--- modul/gejala/gejala.php
+++ modul/gejala/gejala.php
@@ -33,7 +33,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 	<?php
 
 	include "config/fungsi_alert.php";
-	$aksi = "modul/diseaseSaverty/aksi_keparahan.php";
+	$aksi = "modul/gejala/aksi_gejala.php";
 	switch ($_GET['act']) {
     // Tampil hama
 		default:
@@ -43,14 +43,14 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 		if (empty($offset)) {
 			$offset = 0;
 		}
-		$tampil = mysql_query("SELECT * FROM keparahan_penyakit ORDER BY id_keparahan");
-		echo "<form method=POST action='?module=diseaseSaverty' name=text_form onsubmit='return Blank_TextField_Validator_Cari()'>
+		$tampil = mysql_query("SELECT * FROM gejala ORDER BY kode_gejala");
+		echo "<form method=POST action='?module=gejala' name=text_form onsubmit='return Blank_TextField_Validator_Cari()'>
 		<br><br><table class='table table-bordered'>
-		<tr><td><input class='btn bg-olive margin' type=button name=tambah value='Tambah hama' onclick=\"window.location.href='diseaseSaverty/tambahkeparahan';\"><input type=text name='keyword' style='margin-left: 10px;' placeholder='Ketik dan tekan cari...' class='form-control' value='$_POST[keyword]' /> <input class='btn bg-olive margin' type=submit value='   Cari   ' name=Go></td> </tr>
+		<tr><td><input class='btn bg-olive margin' type=button name=tambah value='Tambah hama' onclick=\"window.location.href='gejala/tambahkeparahan';\"><input type=text name='keyword' style='margin-left: 10px;' placeholder='Ketik dan tekan cari...' class='form-control' value='$_POST[keyword]' /> <input class='btn bg-olive margin' type=submit value='   Cari   ' name=Go></td> </tr>
 		</table></form>";
 		$baris = mysql_num_rows($tampil);
 		if ($_POST['Go']) {
-			$numrows = mysql_num_rows(mysql_query("SELECT * FROM keparahan_penyakit where rekomendasi like '%$_POST[keyword]%'"));
+			$numrows = mysql_num_rows(mysql_query("SELECT * FROM gejala where nama_gejala like '%$_POST[keyword]%'"));
 			if ($numrows > 0) {
 				echo "<div class='alert alert-success alert-dismissible'>
 				<h4><i class='icon fa fa-check'></i> Sukses!</h4>
@@ -61,15 +61,12 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 				<thead>
 				<tr>
 				<th>No</th>
-				<th>Luas Daun Terserang</th>
-				<th>Skor</th>
-				<th>Jumlah Daun Skor Sama</th>
-				<th>Rekomendasi</th>
+				<th>Nama Gejala</th>
 				<th>Aksi</th>
 				</tr>
 				</thead>
 				<tbody>";
-				$hasil = mysql_query("SELECT * FROM keparahan_penyakit where rekomendasi like '%$_POST[keyword]%'");
+				$hasil = mysql_query("SELECT * FROM gejala where nama_gejala like '%$_POST[keyword]%'");
 				$no = 1;
 				$counter = 1;
 				while ($r = mysql_fetch_array($hasil)) {
@@ -80,15 +77,12 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 					echo "<tr class='" . $warna . "'>
 
 					<td align=center>$no</td>
-					<td>$r[luas_daun_terserang]</td>
-					<td>$r[skor]</td>
-					<td>$r[jml_daun_skor_sama]</td>
-					<td>$r[rekomendasi]</td>
+					<td>$r[nama_gejala]</td>
 
 
-					<td align=center><a type='button' class='btn btn-block btn-success' href=diseaseSaverty/editkeparahan/$r[id_keparahan]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
+					<td align=center><a type='button' class='btn btn-block btn-success' href=gejala/editkeparahan/$r[kode_gejala]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
 
-					<a type='button' class='btn btn-block btn-danger' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=diseaseSaverty&act=hapus&id=$r[id_keparahan]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\"> <i class='fa fa-trash-o' aria-hidden='true'></i> Hapus</a>
+					<a type='button' class='btn btn-block btn-danger' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=gejala&act=hapus&id=$r[kode_gejala]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\"> <i class='fa fa-trash-o' aria-hidden='true'></i> Hapus</a>
 					</td></tr>";
 					$no++;
 					$counter++;
@@ -109,10 +103,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 				<tr>
 
 				<th>No</th>
-				<th>L.DaunTerserang</th>
-				<th>Skor</th>
-				<th>Jumlah Daun Dengan Skor Sama</th>
-				<th>Rekomendasi</th>
+				<th>Nama Gejala</th>
 				<th>Aksi</th>
 
 
@@ -121,7 +112,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 				</thead>
 				<tbody>
 				";
-				$hasil = mysql_query("SELECT * FROM keparahan_penyakit ORDER BY id_keparahan limit $offset,$limit");
+				$hasil = mysql_query("SELECT * FROM gejala ORDER BY kode_gejala limit $offset,$limit");
 				$no = 1;
 				$no = 1 + $offset;
 				$counter = 1;
@@ -133,15 +124,12 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 					echo "<tr class='" . $warna . "'>
 
 					<td align=center>$no</td>
-					<td>$r[luas_daun_terserang]</td>
-					<td>$r[skor]</td>
-					<td>$r[jml_daun_skor_sama]</td>
-					<td>$r[rekomendasi]</td>
+					<td>$r[nama_gejala]</td>
 					<td align=center>
 
-					<a type='button' class='btn btn-block btn-success' href=diseaseSaverty/editkeparahan/$r[id_keparahan]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
+					<a type='button' class='btn btn-block btn-success' href=gejala/editkeparahan/$r[kode_gejala]><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Ubah </a> &nbsp;
 
-					<a type='button' class='btn btn-block btn-danger' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=diseaseSaverty&act=hapus&id=$r[id_keparahan]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\">
+					<a type='button' class='btn btn-block btn-danger' href=\"JavaScript: confirmIt('Anda yakin akan menghapusnya ?','$aksi?module=gejala&act=hapus&id=$r[kode_gejala]','','','','u','n','Self','Self')\" onMouseOver=\"self.status=''; return true\" onMouseOut=\"self.status=''; return true\">
 
 					<i class='fa fa-trash-o' aria-hidden='true'></i> Hapus</a>
 					</td></tr>";
@@ -154,7 +142,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 
 				if ($offset != 0) {
 					$prevoffset = $offset - 10;
-					echo "<span class=prevnext> <a href=index.php?module=diseaseSaverty&offset=$prevoffset>Back</a></span>";
+					echo "<span class=prevnext> <a href=index.php?module=gejala&offset=$prevoffset>Back</a></span>";
 				} else {
             echo "<span class=disabled>Back</span>"; //cetak halaman tanpa link
           }
@@ -167,7 +155,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
           for ($i = 1; $i <= $halaman; $i++) {
           	$newoffset = $limit * ($i - 1);
           	if ($offset != $newoffset) {
-          		echo "<a href=index.php?module=diseaseSaverty&offset=$newoffset>$i</a>";
+          		echo "<a href=index.php?module=gejala&offset=$newoffset>$i</a>";
               //cetak halaman
           	} else {
               echo "<span class=current>" . $i . "</span>"; //cetak halaman tanpa link
@@ -179,7 +167,7 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 
             //jika bukan halaman terakhir maka berikan next
           	$newoffset = $offset + $limit;
-          	echo "<span class=prevnext><a href=index.php?module=diseaseSaverty&offset=$newoffset>Next</a>";
+          	echo "<span class=prevnext><a href=index.php?module=gejala&offset=$newoffset>Next</a>";
           } else {
             echo "<span class=disabled>Next</span>"; //cetak halaman tanpa link
           }
@@ -193,51 +181,31 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
 
 
       case "tambahkeparahan":
-      echo "<form name=text_form method=POST action='$aksi?module=diseaseSaverty&act=input' onsubmit='return Blank_TextField_Validator()' enctype='multipart/form-data'>
+      echo "<form name=text_form method=POST action='$aksi?module=gejala&act=input' onsubmit='return Blank_TextField_Validator()' enctype='multipart/form-data'>
       <br><br><table class='table table-bordered'>
 
 
       <tr>
-      <td width=120>Luas Daun Terserang</td>
+      <td width=120>Nama Gejala</td>
       <td>
-      <input autocomplete='off' type=text placeholder='Masukkan hama baru...' class='form-control' name='luas_daun_terserang' size=30>
+      <input autocomplete='off' type=text placeholder='Masukkan hama baru...' class='form-control' name='nama_gejala' size=30>
       </td>
       </tr>
 
-      <tr>
-      <td width=120>Skor</td>
-      <td>
-      <input autocomplete='off' type=text placeholder='Masukkan hama baru...' class='form-control' name='skor' size=30>
-      </td>
-      </tr>
-
-      <tr>
-      <td width=120>Jumlah Daun Terserang</td>
-      <td>
-      <input autocomplete='off' type=text placeholder='Masukkan hama baru...' class='form-control' name='jml_daun_skor_sama' size=30>
-      </td>
-      </tr>
-
-      <tr>
-      <td width=120>Rekomendasi</td>
-      <td>
-      <input autocomplete='off' type=text placeholder='Masukkan hama baru...' class='form-control' name='rekomendasi' size=30>
-      </td>
-      </tr>
 
 
 
       <tr><td></td><td><input class='btn btn-danger' type=submit name=submit value='Simpan' >
 
 
-      <input class='btn btn-success' type=button name=batal value='Batal' onclick=\"window.location.href='?module=diseaseSaverty';\"></td></tr>
+      <input class='btn btn-success' type=button name=batal value='Batal' onclick=\"window.location.href='?module=gejala';\"></td></tr>
       </table></form>";
       break;
 
 
 
       case "editkeparahan":
-      $edit = mysql_query("SELECT * FROM keparahan_penyakit WHERE id_keparahan='$_GET[id]'");
+      $edit = mysql_query("SELECT * FROM gejala WHERE kode_gejala='$_GET[id]'");
       $r = mysql_fetch_array($edit);
       // if ($r['gambar']) {
       // 	$gambar = 'gambar/hama/' . $r['gambar'];
@@ -245,19 +213,15 @@ if (!(isset($_SESSION['username']) && isset($_SESSION['password']))) {
       // 	$gambar = 'gambar/noimage.png';
       // }
 
-      echo "<form name=text_form method=POST action='$aksi?module=diseaseSaverty&act=update' onsubmit='return Blank_TextField_Validator()' enctype='multipart/form-data'>
-      <input type=hidden name=id value='$r[id_keparahan]'>
+      echo "<form name=text_form method=POST action='$aksi?module=gejala&act=update' onsubmit='return Blank_TextField_Validator()' enctype='multipart/form-data'>
+      <input type=hidden name=id value='$r[kode_gejala]'>
       <br><br><table class='table table-bordered'>
-      <tr><td width=120>Luas Daun Terserang</td><td><input autocomplete='off' type=text class='form-control' name='luas_daun_terserang' size=30 value=\"$r[luas_daun_terserang]\"></td></tr>
-      <tr><td width=120>SKor</td><td><textarea rows='4' cols='50' type=text class='form-control' name='skor'>$r[skor]</textarea></td></tr>
-
-      <tr><td width=120>Jumlah Daun Skor Sama</td><td><textarea rows='4' cols='50' type=text class='form-control' name='jml_daun_skor_sama'>$r[jml_daun_skor_sama]</textarea></td></tr>
-
-      <tr><td width=120>Rekomendasi</td><td><textarea rows='4' cols='50' type=text class='form-control' name='rekomendasi'>$r[rekomendasi]</textarea></td></tr>
-
+      <tr><td width=120>Luas Daun Terserang</td><td><input autocomplete='off' type=text class='form-control' name='nama_gejala' size=30 value=\"$r[nama_gejala]\"></td>
+      </tr>
+      
       <tr><td></td><td><input class='btn btn-success' type=submit name=submit value='Simpan' >
 
-      <input class='btn btn-danger' type=button name=batal value='Batal' onclick=\"window.location.href='?module=diseaseSaverty';\"></td></tr>
+      <input class='btn btn-danger' type=button name=batal value='Batal' onclick=\"window.location.href='?module=gejala';\"></td></tr>
       </table></form>";
       break;
     }
